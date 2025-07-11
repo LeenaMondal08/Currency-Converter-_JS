0c4f3cf1cfd318107f58735f94958568bb41c19a
LeenaMondal08 <leenamondal1813@gmail.com>
2025-07-11 07:45:26 +0530

    fixed issue in select option

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const BASE_URL= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
-const dropdrowns=document.querySelectorAll(".dropdrown select");
+const dropdrowns=document.querySelectorAll(".dropdown select");
 
 for(let select of dropdrowns){
     for(currCode in countryList){
         let newOption=document.createElement("option");
         newOption.innerText=currCode;
         newOption.value=currCode;
-        select.appendChild(newOption);
+        select.append(newOption);
     }
 }
